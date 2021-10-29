b0160bfdd9d30d5be46369bbe8bb8f94fadc01d9
Erik Vicente <erikdelimvicente@gmail.com>
2021-10-29 09:53:30 -0300

    first commit all files

---
+++ terraform-dok/simple/variables.tf
@@ -0,0 +1,26 @@
+variable "pool_size" {
+  type    = string
+  default = "s-2vcpu-4gb"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.20.7-do.0"
+}
+
+variable "ingress_nginx" {
+  type    = bool
+  default = false
+}
+
+variable "min_nodes" {
+  type    = number
+  default = 3
+}
+
+variable "max_nodes" {
+  type    = number
+  default = 6
+}
+
+
